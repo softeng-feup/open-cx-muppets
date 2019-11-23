0183dd06826b221c2e753faa6c55dde9e03cfba9
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-11-23 14:50:13 +0000

    Database Mockup with main methods

---
+++ app/lib/Database/Database.dart
@@ -0,0 +1,104 @@
+import 'dart:async';
+import 'package:app/Database/Friends.dart';
+import 'package:app/Database/User.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+//Database scripts to promote encapsulation
+
+// Singleton class to mock the database
+class MMDatabase {
+  static final MMDatabase _instance = MMDatabase._internal();
+  String _path;
+  Database _database;
+
+  factory MMDatabase() {
+    return _instance;
+  }
+
+  MMDatabase._internal() {
+    _loadPath();
+    _deleteDb();
+    _initDb();
+    _populateDb();
+  }
+
+  Future<void> _loadPath() async {
+    String _databasesPath = await getDatabasesPath();
+    _path = join(_databasesPath, 'demo.db');
+  }
+
+  Future<void> _deleteDb() async {
+    await deleteDatabase(_path);
+  }
+
+  Future<void> _initDb() async {
+    _database = await openDatabase(_path, version: 1,
+        onCreate: (Database db, int version) async {
+      // When creating the db, create the tables
+      await db.execute(
+          'CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, nationality TEXT,occupation TEXT, company TEXT, languages TEXT, contacts TEXT, interests TEXT)');
+      await db.execute(
+          'CREATE TABLE friends (user_id INTEGER , friend_id Integer, PRIMARY KEY(user_id, friend_id), FOREIGN KEY (user_id) REFERENCES User(id), FOREIGN KEY (friend_id) REFERENCES User(id))');
+    });
+  }
+
+  Future<void> _populateDb() async {
+    List<User> users = <User>[];
+    List<Friends> friends = <Friends>[];
+
+    users.add(User(
+      id: 1,
+      name: 'John Doe',
+      nationality: 'North American',
+      occupation: 'IT consultant',
+      company: 'SomeInk Software',
+      languages: 'English',
+      contacts: ['123', 'johnemail@smh.com'],
+      interests: ['javascript', 'iot', 'software engineering'],
+    ));
+    users.add(User(
+      id: 2,
+      name: 'Ana Amarelo',
+      nationality: 'Portuguese',
+      occupation: 'Scientist',
+      company: 'CGI Enterprise',
+      languages: 'Portuguese',
+      contacts: ['456', 'anaemail@smh.com'],
+      interests: ['python', 'research', 'quantum computing'],
+    ));
+    users.add(User(
+      id: 3,
+      name: 'Red Resnikov',
+      nationality: 'Russian',
+      occupation: 'Cybersecurity Expert',
+      company: 'CyberWall',
+      languages: 'Russian',
+      contacts: ['789', 'redemail@smh.com'],
+      interests: ['javascript', 'database management', 'web development'],
+    ));
+
+    friends.add(Friends(user_id: 1, friend_id: 2));
+    friends.add(Friends(user_id: 1, friend_id: 3));
+    friends.add(Friends(user_id: 2, friend_id: 1));
+    friends.add(Friends(user_id: 3, friend_id: 1));
+
+    // Insert the User into the correct table. Also specify the
+    // `conflictAlgorithm`. In this case, if the same user is inserted
+    // multiple times, it replaces the previous data.
+    users.forEach((user) async {
+      await _database.insert(
+        'users',
+        user.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+    });
+
+    friends.forEach((friend) async {
+      await _database.insert(
+        'friends',
+        friend.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+    });
+  }
+}
--- app/lib/Database/User.dart
+++ app/lib/Database/User.dart
@@ -5,8 +5,8 @@ class User {
   final String occupation;
   final String company;
   final String languages;
-  final String contacts; //CSV contact list
-  final String interests;
+  final List<String> contacts;
+  final List<String> interests;
 
   User(
       {this.id,
@@ -18,18 +18,31 @@ class User {
       this.contacts,
       this.interests});
 
+  String listToCSV(List<String> list) {
+    String csv = '';
+    if (list.isNotEmpty) {
+      csv = list.elementAt(0);
+
+      for (int i = 1; i < list.length; i++) {
+        csv += ',' + list.elementAt(i);
+      }
+    }
+
+    return csv;
+  }
+
   // Convert a uSER into a Map. The keys must correspond to the names of the
   // columns in the database.
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'name': name,
-      'nationality' : nationality,
-      'occupation' : occupation,
-      'company' : company,
-      'languages' : languages,
-      'contacts' : contacts,
-      'interests' : interests
+      'nationality': nationality,
+      'occupation': occupation,
+      'company': company,
+      'languages': languages,
+      'contacts': listToCSV(contacts),
+      'interests': listToCSV(interests)
     };
   }
 }
